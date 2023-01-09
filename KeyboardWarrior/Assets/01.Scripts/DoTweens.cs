4d9d75f8d82db4e1e227a196f607ec6898f313d5
SungJieundev <sungjieun.dev@gmail.com>
2023-01-10 03:10:18 +0900

    dotween TMP

--- KeyboardWarrior/Assets/01.Scripts/DoTweens.cs
+++ KeyboardWarrior/Assets/01.Scripts/DoTweens.cs
@@ -1,29 +1,28 @@
-// using System.Net.Mime;
-// using System.ComponentModel;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using DG.Tweening;
-// using TMPro;
+using System.Net.Mime;
+using System.ComponentModel;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
-// public class DoTweens : MonoBehaviour
-// {
-//     public static DoTweens instance;
+public class DoTweens : MonoBehaviour
+{
+    public static DoTweens instance;
 
-//     private void Awake() {
-//         if (!instance) instance = this;
-//         else { Destroy(gameObject); }
-//     }
+    private void Awake() {
+        if (!instance) instance = this;
+        else { Destroy(gameObject); }
+    }
 
 
-//     // private string myString;
-//     public void DoString(string msg, TextMeshProUGUI txt, float time) { //글자 타닥타닥 해주는 거
-//         // txt.GetComponent<text>().d);
-//         // DOTween.To(() => "", str => myString = str, "tlqkf", 3);
-//     }
+    public void DoString(string msg, TextMeshProUGUI txt, float time) { //글자 타닥타닥 해주는 거        
 
-//     public void DoTColor(GameObject obj, Color color, float time) {
+        txt.DOText(msg, time);
+    }
 
-//         obj.GetComponent<Material>().DOColor(color, time);
-//     }
-// }
+    public void DoTColor(GameObject obj, Color color, float time) {
+
+        obj.GetComponent<Material>().DOColor(color, time);
+    }
+}
