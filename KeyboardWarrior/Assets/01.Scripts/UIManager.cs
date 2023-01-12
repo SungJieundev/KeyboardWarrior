b289f0c0480075d1893a8deac2860d33ccaacd3f
SungJieundev <sungjieun.dev@gmail.com>
2023-01-12 21:00:28 +0900

    asdfb

--- KeyboardWarrior/Assets/01.Scripts/UIManager.cs
+++ KeyboardWarrior/Assets/01.Scripts/UIManager.cs
@@ -8,13 +8,20 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreTxt;
 
     [SerializeField] private TextMeshProUGUI maxScoreTxt;   
+
+    private DoTweens doTweens;
+    
+    public GameObject escPanel;
     public int curScore = 0;
 
     public int addScore = 0;
 
     private int maxScore = 0;
 
+    public bool isPressedESC = false;
+
     private void Awake() {
+
 
         scoreTxt.text = "Score : 0";
     }
@@ -24,5 +31,19 @@ public class UIManager : MonoBehaviour
         // if (curScore >= maxScore) maxScoreTxt.text = $"Max Score : {maxScore}";
         scoreTxt.text = $"Score : {curScore}";
 
+        if(Input.GetKeyDown(KeyCode.Escape)){
+
+            if(isPressedESC == true){
+
+                doTweens.PanelUp(escPanel);
+                isPressedESC = false;
+            }
+
+            if(isPressedESC == false){
+
+                doTweens.PanelDown(escPanel);
+                isPressedESC = true;
+            }
+        }
     }
 }
